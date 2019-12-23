baeecd0c8d63a728c96cd17ed51393d39f073223
jfpx <joyit@126.com>
2019-12-23 12:04:31 -0800

    tunnel multiple ports

--- dockerstartup/entrypoint.sh
+++ dockerstartup/entrypoint.sh
@@ -20,10 +20,29 @@ fi
 
 if [ -n "${TOKEN}" ]; then
   echo "create tunnel to ngrok, no port need to open in container, use following admin login to access"
-  ngrok http -auth="admin:${SUDO_PASSWORD}" -authtoken ${TOKEN} ${PORT} > /dev/null &
+  
+  echo "authtoken: $TOKEN" >> ${HOME}/ngrok.yml
+  echo "tunnels:" >> ${HOME}/ngrok.yml
+  echo "  codeserver:" >> ${HOME}/ngrok.yml
+  echo "    proto: http" >> ${HOME}/ngrok.yml
+  echo "    addr: 8443" >> ${HOME}/ngrok.yml
+  echo "    auth: \"admin:${SUDO_PASSWORD}\"" >> ${HOME}/ngrok.yml
+  echo "  vnc:" >> ${HOME}/ngrok.yml
+  echo "    proto: http" >> ${HOME}/ngrok.yml
+  echo "    addr: 6901" >> ${HOME}/ngrok.yml
+  echo "    auth: \"admin:${SUDO_PASSWORD}\"" >> ${HOME}/ngrok.yml
+  ngrok start -config ${HOME}/ngrok.yml --all > /dev/null &
+
   sleep 5
-  VSCODEWEB=$(curl http://localhost:4040/api/tunnels | jq -r ".tunnels[0].public_url")
-  echo "NGROK_ADMIN_LOGIN: " $VSCODEWEB
+  echo "list all tunnels:"
+  VSCODEWEB=$(curl --silent http://localhost:4040/api/tunnels | jq -r ".tunnels[0].public_url")
+  curl --silent http://localhost:4040/api/tunnels | jq -r ".tunnels[0].public_url"
+  curl --silent http://localhost:4040/api/tunnels | jq -r ".tunnels[1].public_url"
+  curl --silent http://localhost:4040/api/tunnels | jq -r ".tunnels[2].public_url"
+  curl --silent http://localhost:4040/api/tunnels | jq -r ".tunnels[3].public_url"
+  curl --silent http://localhost:4040/api/tunnels | jq -r ".tunnels[4].public_url"
+  
+  echo "vs code 8443 tunnel: " $VSCODEWEB
   if [ -n "${REDIRECT}" ]; then
     pkill -f redirect
     /dockerstartup/redirect $VSCODEWEB ${REDIRECT} true &  
