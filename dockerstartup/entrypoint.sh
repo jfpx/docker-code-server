79dec68bff5d94ba23f55a1a985b023183c0fe7a
jfpx <joyit@126.com>
2019-12-23 23:46:13 -0800

    use portforward executable built on py35

--- dockerstartup/entrypoint.sh
+++ dockerstartup/entrypoint.sh
@@ -41,9 +41,9 @@ if [ -n "${TOKEN}" ]; then
   #curl --silent http://localhost:4040/api/tunnels | jq -r ".tunnels[1].public_url"
   #echo "vs code 8443 tunnel: " $VSCODEWEB
   if [ -n "${REDIRECT}" ]; then
-    sudo python3 /dockerstartup/portforward.py ${REDIRECT} &
-    #pkill -f redirect 
-    #sudo /dockerstartup/redirect $VSCODEWEB ${REDIRECT} true &  
+    #sudo python3 /dockerstartup/portforward.py ${REDIRECT} &
+    pkill -f portforward
+    sudo /dockerstartup/portforward ${REDIRECT} &
   fi
 else
   echo "token is not provided for ngrok, make sure open port 8443 to access"
