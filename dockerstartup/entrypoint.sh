5eb8c432d747d7a4326f66685f49ee53ec181778
jfpx <joyit@126.com>
2019-12-25 19:16:22 -0800

    use environment variable for password

--- dockerstartup/entrypoint.sh
+++ dockerstartup/entrypoint.sh
@@ -37,7 +37,8 @@ if [ -n "${REDIRECT}" ]; then
   sleep 60
   echo "create tunnel to ngrok, no port need to open in container, use following admin login to access"
   
-  /dockerstartup/ngrokserver config ${TOKEN} ${REDIRECT} admin:${SUDO_PASSWORD} ${HOME}/ngrok.yml
+  USERPWD=admin:$SUDO_PASSWORD
+  /dockerstartup/ngrokserver config $TOKEN $REDIRECT $USERPWD ${HOME}/ngrok.yml
   ngrok start -config ${HOME}/ngrok.yml --all > /dev/null &
   echo "started ngrok service"
   
@@ -51,7 +52,6 @@ if [ -n "${REDIRECT}" ]; then
   curl --silent http://localhost:4040/api/tunnels | jq -r ".tunnels[5].public_url"
   if [ -n "${REDIRECTPORT}" ]; then
     #sudo pkill -f portforward
-    #sudo /dockerstartup/portforward ${REDIRECT} &
     #sudo python3 /dockerstartup/portforward.py ${REDIRECT} &
     sudo /dockerstartup/ngrokserver server ${REDIRECTPORT} ${HOME}/ngrokweb http://localhost:4040/api/tunnels &
     echo "please access web port ${REDIRECTPORT} to access"
